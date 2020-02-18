9cb901bf9ec1233cf4a275cd1b6bde0b5e529ce2
ilarisilander <ilarisilander@hotmail.com>
2020-02-19 00:05:27 +0100

    Add Insert method

--- src/rememberPassword.java
+++ src/rememberPassword.java
@@ -11,15 +11,25 @@ public class rememberPassword {
         printMenu();
 
         while(active) {
-
+            System.out.print("Välj nummer: ");
             int choice = sc.nextInt();
             sc.nextLine();
 
             switch(choice) {
                 case 1:
-                    addAccount();
+                    insertAccount();
                     break;
                 case 2:
+                    updateAccount();
+                    break;
+                case 3:
+                    removeAccount();
+                    break;
+                case 4:
+                    showAllAccounts();
+                    break;
+                case 0:
+                    System.exit(1);
                     break;
             }
 
@@ -37,7 +47,54 @@ public class rememberPassword {
                 "----------------------------");
     }
 
-    private static void addAccount() {
+    private static Connection connect() {
+        String url = "jdbc:sqlite:D:/Javaprojekt/remember-password/database/keypass.db";
+
+        Connection conn = null;
+
+        try{
+            conn = DriverManager.getConnection(url);
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    private static void insertAccount() {
+        String accountSql = "INSERT INTO account(accountLocation, accountUser) VALUES(?,?)";
+        String passSql = "INSERT INTO password(password) VALUES(?)";
+
+        System.out.print("Ange vart kontot finns (hemsida/spel/app): ");
+        String location = sc.nextLine();
+        System.out.print("Ange användarnamn: ");
+        String user = sc.nextLine();
+        System.out.print("Ange lösenord: ");
+        String pass = sc.nextLine();
+
+        try{
+            Connection conn = connect();
+            PreparedStatement prepAcc = conn.prepareStatement(accountSql);
+            PreparedStatement prepPass = conn.prepareStatement(passSql);
+            prepAcc.setString(1, location);
+            prepAcc.setString(2, user);
+            prepPass.setString(1, pass);
+            prepAcc.executeUpdate();
+            prepPass.executeUpdate();
+            System.out.println("Du lade till ett ny konto");
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void updateAccount() {
+
+    }
+
+    private static void removeAccount() {
+
+    }
+
+    private static void showAllAccounts() {
 
     }
 }
